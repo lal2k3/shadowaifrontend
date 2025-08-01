d4048315e0615c2c98f9387e3e079b930f090da1
lal2k3@gmail.com <lal2k3>
2025-08-01 20:30:47 +0300

    removed debugging

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,8 @@ import 'styles/policies.scss';
 import 'styles/login.scss';
 import 'styles/tables.scss';
 import CommunicationInit from 'utils/authentication/CommunicationInit';
-import { AppConfig } from 'components/general/AppConfig';
 
 function App() {
-  console.log(AppConfig.serverUrl);
   return (
     <>
       <CommunicationInit />
